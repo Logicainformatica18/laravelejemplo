71d93608863fb43a92f41fa4fdafd789b184b07d
Anthony <logicainformatica18@gmail.com>
2020-09-29 18:11:16 -0500

    mandando slaudo

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('saludo', function () {
+    return "hola weeeeeeee";
+});
 Route::get('home', function () {
     return view('template');
 });
